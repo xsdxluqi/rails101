7459f1a5a7422a5113d8f9fc8430e54992fef8e8
liuqi <xsdxluqi3class@gmail.com>
2016-12-29 13:37:43 -0800

    add validetion to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,9 +17,12 @@ def new
 
 def create
   @group = Group.new(group_params)
-   @group.save
 
-     redirect_to groups_path
+   if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
  end
 
  def update
