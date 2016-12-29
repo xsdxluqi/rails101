533980048970da99a5122bd3ac8947cc6c70ff82
liuqi <xsdxluqi3class@gmail.com>
2016-12-29 15:06:50 -0800

    add permission check on show page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
     @groups = Group.all
@@ -9,15 +10,14 @@ class GroupsController < ApplicationController
    @group = Group.find(params[:id])
  end
 
- def edit
-    @group = Group.find(params[:id])
+  def edit
   end
 
-def new
+ def new
    @group = Group.new
  end
 
-def create
+ def create
   @group = Group.new(group_params)
   @group.user = current_user
 
@@ -28,9 +28,7 @@ def create
     end
  end
 
- def update
-    @group = Group.find(params[:id])
-
+   def update
     if @group.update(group_params)
    redirect_to groups_path, notice: "Update Success"
  else
@@ -38,16 +36,22 @@ def create
   end
 end
 
-  def destroy
-    @group = Group.find(params[:id])
+   def destroy
     @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
+    redirect_to groups_path, alert: "Group deleted"
   end
 
 
 
  private
+
+ def find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
  def group_params
    params.require(:group).permit(:title, :description)
